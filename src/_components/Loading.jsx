a32bc5fc2fc83c324bd536ccd2a401fcc03c68ac
Nano <fmetitiere@gmail.com>
2019-08-10 15:01:54 -0300

    NANOWEBAPP-02: Se agrego IsMobile Component

--- src/_components/Loading.jsx
+++ src/_components/Loading.jsx
@@ -1,48 +1,40 @@
-import React from "react";
-import FadeIn from "react-fade-in";
-import Lottie from "react-lottie";
-import ReactLoading from "react-loading";
-import "bootstrap/dist/css/bootstrap.css";
+import React from 'react';
+import FadeIn from 'react-fade-in';
+import Lottie from 'react-lottie';
+
+import { BrowserView, MobileView } from 'react-device-detect';
+
+import 'bootstrap/dist/css/bootstrap.css';
 import Home from './Home';
 
 import * as animationData from '../Animations/anim1.json';
 
 const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData.default,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  };
+	loop: true,
+	autoplay: true,
+	animationData: animationData.default,
+	rendererSettings: {
+		preserveAspectRatio: 'xMidYMid slice'
+	}
+};
 
 export default class Loading extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      done: undefined
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			done: undefined
+		};
+	}
 
-  componentDidMount() {
-    setTimeout(() => {
-      fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(response => response.json())
-        .then(json => this.setState({ done: true }));
-    }, 1200);
-  }
+	componentDidMount() {
+		setTimeout(() => {
+			fetch('https://jsonplaceholder.typicode.com/posts')
+				.then((response) => response.json())
+				.then((json) => this.setState({ done: true }));
+		}, 1500);
+	}
 
-  render() {
-    return (
-      <div>
-        {!this.state.done ? (
-          <Lottie options={defaultOptions}
-          isStopped={this.state.isStopped}
-          isPaused={this.state.isPaused}/>
-        ) : (
-          <Home/>
-        )}
-      </div>
-    );
-  }
+	render() {
+		return <div>{!this.state.done ? <Lottie options={defaultOptions} height={420} width={420} /> : <Home />}</div>;
+	}
 }
